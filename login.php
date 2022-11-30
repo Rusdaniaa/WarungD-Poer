61126ce76e81e47a7709e688b3aaf32bf44aaa14
Rusdaniaa <sitirusdania5244@gmail.com>
2022-11-30 19:02:28 +0700

    login, homr, table

--- login.php
+++ login.php
@@ -6,7 +6,6 @@ session_start();
 if (isset($_POST['submit'])){
     $email = $_POST["txt_email"];
     $pass = $_POST['txt_pass'];
-    #rasigahhmaa
 
     /*
     $emailCheck = mysqli_real_escape_string($koneksi, $email);
@@ -15,15 +14,15 @@ if (isset($_POST['submit'])){
 
 if(!empty(trim($email)) && !empty(trim($pass))){
     //select data berdasarkan username dari database 
-    $query = "SELECT *FROM user_detail WHERE user_email = '$email'";
+    $query = "SELECT *FROM user WHERE email = '$email'";
     $result = mysqli_query($konek, $query);
     $num = mysqli_num_rows($result);
 
     while( $row = mysqli_fetch_array($result)){
-        $id = $row ['id'];
-        $UserVal = $row ['user_email'];
-        $passVal = $row ['user_password'];
-        $userName = $row['user_fullname'];
+        $id = $row ['id_user'];
+        $UserVal = $row ['email'];
+        $passVal = $row ['password'];
+        $userName = $row['nama_user'];
         $level = $row ['level'];
     }
     if ($num != 0){
@@ -35,7 +34,7 @@ if(!empty(trim($email)) && !empty(trim($pass))){
             header('Location: home.php');
         }else{
             $error = 'user atau password salah!';
-            $header ('Location:login.php');
+            header ('Location:login.php');
         }
     }else{
         $error = 'user atau password salah!!';
@@ -47,15 +46,92 @@ if(!empty(trim($email)) && !empty(trim($pass))){
 }
 }
 ?>
-<html>
-    <head>
-        <title>login page</title>
-    </head>
-    <body>
-        <form action="login.php" method="POST">
-            <p>email &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <input type="text" name="txt_email"></p>
-            <p>password : <input type="password" name="txt_pass"></p>
-            <button type="submit" name="submit">Sign In</button>
-        </form>
-    </body>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <form class="user" action="login.php" method="POST">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address..." name="txt_email">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Password" name="txt_pass">
+                                        </div>
+                                        <button type="submit" name="submit" class="btn btn-primary btn-user btn-block">Login</button>
+
+                                        <hr>
+                                        
+                                    </form>
+                                    <hr>
+                                    
+                                    <div class="text-center">
+                                        <a class="small" href="register.php">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
 </html>
